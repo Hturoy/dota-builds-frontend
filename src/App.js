7f80c393b1a08b613a1678f22b1d8e980a256b8f
Hturoy <69842773+Hturoy@users.noreply.github.com>
2022-02-13 23:13:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL, HEROES, POPULATE } from "./utils/api";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(BASE_URL + HEROES + POPULATE)
+      .then((response) => setData(response.data.data));
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello1!</h1>
+      <ul>
+        {data.length > 0
+          ? data.map((hero, idx) => {
+              return <li key={idx}>{hero.attributes.name}</li>;
+            })
+          : null}
+      </ul>
+    </>
   );
 }
-
 export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "https://dota-builds.herokuapp.com/api/";
+export const POPULATE = "?populate=*";
+export const HEROES = "dota-heroes/";
+export const BUILDS = "dota-builds/";
+export const ITEMS = "items/";
