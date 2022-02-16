e6cda9def751ead9654b0233265571a466fcc00b
Hturoy <69842773+Hturoy@users.noreply.github.com>
2022-02-16 14:34:18 +0100

    set up basic hero card

--- src/App.js
+++ src/App.js
@@ -11,13 +11,31 @@ function App() {
       .then((response) => setData(response.data.data));
   });
 
+  //hero.attributes.name
+  //hero.attributes.dota_build.data.attributes.core1
+  //hero.attributes.image.data.attributes.name
+
   return (
     <>
-      <h1>Hello1!</h1>
+      <h1 className="text-3xl font-bold underline">Hello1!</h1>
       <ul>
         {data.length > 0
           ? data.map((hero, idx) => {
-              return <li key={idx}>{hero.attributes.name}</li>;
+              return (
+                <div key={idx}>
+                  <img
+                    src={hero.attributes.image.data.attributes.name}
+                    alt="Image of dota hero"
+                  />
+                  <h2>{hero.attributes.name}</h2>
+                  <ul>
+                    <li>{hero.attributes.role}</li>
+                    <li>{hero.attributes.type}</li>
+                    <li>{hero.attributes.difficulty}</li>
+                  </ul>
+                  <p>{hero.attributes.description}</p>
+                </div>
+              );
             })
           : null}
       </ul>
